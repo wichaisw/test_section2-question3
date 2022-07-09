a5b66244309d6e3efb332ec93b2beba25dba3ceb
wichaisw <wichai.sw19@gmail.com>
2022-07-09 21:37:13 +0700

    refactor: add exit code, remove anonymous function name

--- index.js
+++ index.js
@@ -3,14 +3,13 @@ const { TABLE_EL } = require('./utils/constants.js');
 const { scrapeController } = require('./services/element-scrapper.js');
 const { extractNavFromSymbol } = require('./services/data-cleaner.js');
 
-
 let scriptArg = argv[2];
 if(!scriptArg) {
   console.log('please insert a fund name')
-  process.exit();
+  process.exit(9);
 }
 
-(async function waitForScraper() {
+(async function() {
   // node support top-level await since v14
   const data = await scrapeController('https://codequiz.azurewebsites.net/', '', TABLE_EL);
   const res = extractNavFromSymbol(data, scriptArg);
